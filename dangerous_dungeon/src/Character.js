ba47c4d6651565d1b92587b6837300d0d757b37b
Nikolaev Nikita <69678682+NikitkaN@users.noreply.github.com>
2023-04-20 00:55:09 +0300

    New

--- dangerous_dungeon/src/Character.js
+++ dangerous_dungeon/src/Character.js
@@ -1,17 +1,34 @@
-import Oko from './img/Oko.png'
+import * as THREE from 'three'
+// import Oko from './img/Oko.png'
+import God from './img/God.png'
 import { Suspense } from 'react'
 import { useLoader } from '@react-three/fiber'
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
+import { useTexture } from '@react-three/drei'
 
 function Image() {
-    const texture = useLoader(TextureLoader, Oko)
+    const texture = useLoader(THREE.TextureLoader, '/Oko.png')
+    texture.repeat.set(1, 1)
+    texture.offset.x = 0
+    texture.offset.y = 0
+
     return (
       <mesh>
-        <planeBufferGeometry attach="geometry" args={[125, 250]} />
-        <meshBasicMaterial attach="material" map={texture} toneMapped={false} />
+        <planeBufferGeometry args={[121, 273]} />
+        <meshBasicMaterial map={texture} toneMapped={false} />
       </mesh>
     )
   }
+
+// function Image() {
+//   const texture = useTexture(Oko)
+//   return (
+//           <mesh>
+//             <planeBufferGeometry attach="geometry" args={[121, 273]} />
+//             <meshBasicMaterial attach="material" map={texture} toneMapped={false} />
+//           </mesh>
+//   )
+// }
 
 const Character = (props) => {
     return (
