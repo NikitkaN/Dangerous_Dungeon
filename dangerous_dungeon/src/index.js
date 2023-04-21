778a6e9a337b31b250b04bb96458671605a8dfcb
Lowell Dank <lowellda@ev-g5.kzn.21-school.ru>
2023-04-21 14:11:19 +0300

    New

--- dangerous_dungeon/src/index.js
+++ dangerous_dungeon/src/index.js
@@ -9,16 +9,13 @@ import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-  // <Provider store={store}>
-  //   <React.StrictMode>
-  //     <App />
-  //   </React.StrictMode>
-  // </Provider>
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
